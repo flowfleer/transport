0f76bc4425564b33a6ef8716c4ade5aad89ea785
Raj <raj.ghodasara04@gmail.com>
2023-02-04 13:45:02 +0530

    final changes

---
+++ Components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+        <div className='flex justify-center items-center'>
+            <hr className='border-2 w-4/5'/>
+        </div>
+        <div className='text-center mt-7 text-sans text-sm md:text-base lg:text-lg xl:text-xl'>
+            <p className='text-4xl xl:text-5xl text-primary-dark mb-5'>CONTACT US</p>
+            <p className='flex justify-center'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" height='1.2rem' width='1.2rem' className='m-[1px]'><path fill="#23D997" d="M12,10.8a2,2,0,1,0-2-2A2,2,0,0,0,12,10.8Zm-.71,6.91a1,1,0,0,0,1.42,0l4.09-4.1a6.79,6.79,0,1,0-9.6,0ZM7.23,8.34A4.81,4.81,0,0,1,9.36,4.79a4.81,4.81,0,0,1,5.28,0,4.82,4.82,0,0,1,.75,7.41L12,15.59,8.61,12.2A4.77,4.77,0,0,1,7.23,8.34ZM19,20H5a1,1,0,0,0,0,2H19a1,1,0,0,0,0-2Z"/></svg>: 92, Mahadev Nagar, Varachha Road, Surat (India) -395006</p>
+            <p className='flex justify-center text-sans'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" height='1.2rem' width='1.2rem' className='m-[1px]'><path fill="#23D997" d="M19.44,13c-.22,0-.45-.07-.67-.12a9.44,9.44,0,0,1-1.31-.39,2,2,0,0,0-2.48,1l-.22.45a12.18,12.18,0,0,1-2.66-2,12.18,12.18,0,0,1-2-2.66L10.52,9a2,2,0,0,0,1-2.48,10.33,10.33,0,0,1-.39-1.31c-.05-.22-.09-.45-.12-.68a3,3,0,0,0-3-2.49h-3a3,3,0,0,0-3,3.41A19,19,0,0,0,18.53,21.91l.38,0a3,3,0,0,0,2-.76,3,3,0,0,0,1-2.25v-3A3,3,0,0,0,19.44,13Zm.5,6a1,1,0,0,1-.34.75,1.05,1.05,0,0,1-.82.25A17,17,0,0,1,4.07,5.22a1.09,1.09,0,0,1,.25-.82,1,1,0,0,1,.75-.34h3a1,1,0,0,1,1,.79q.06.41.15.81a11.12,11.12,0,0,0,.46,1.55l-1.4.65a1,1,0,0,0-.49,1.33,14.49,14.49,0,0,0,7,7,1,1,0,0,0,.76,0,1,1,0,0,0,.57-.52l.62-1.4a13.69,13.69,0,0,0,1.58.46q.4.09.81.15a1,1,0,0,1,.79,1Z"/></svg>: +91 8980083383 , +91 9099823291</p>
+            <p className='flex justify-center text-sans mb-5'><svg xmlns="http://www.w3.org/2000/svg" data-name="Layer 1" viewBox="0 0 24 24" height='1.2rem' width='1.2rem' className='m-[1px]'><path fill="#23D997" d="M14,11.51h2.42a1,1,0,0,0,.71-.29l4.58-4.58a1,1,0,0,0,0-1.42L19.29,2.8a1,1,0,0,0-1.42,0L13.29,7.38a1.05,1.05,0,0,0-.29.71v2.42A1,1,0,0,0,14,11.51Zm1-3,3.58-3.58,1,1L16,9.51H15Zm6,2a1,1,0,0,0-1,1v7a1,1,0,0,1-1,1H5a1,1,0,0,1-1-1V8.9l5.88,5.89a3,3,0,0,0,4.27,0,1,1,0,0,0,0-1.4,1,1,0,0,0-1.43,0,1,1,0,0,1-1.4,0L5.41,7.49H10a1,1,0,0,0,0-2H5a3,3,0,0,0-3,3v10a3,3,0,0,0,3,3H19a3,3,0,0,0,3-3v-7A1,1,0,0,0,21,10.49Z"/></svg>: advaithexport@gmail.com</p>
+        </div>
+    </>
+  )
+}
+
+export default Footer
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,19 @@
-import Layout from '../Components/Layout'
+import Footer from '../Components/Footer'
+import Info from '../Components/Info'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+function MyApp() {
+
+  const obj={
+    first_p : "We are a leading business in the field of import and export, based in India. Our motto is to connect with lives of people by providing products of their need to enhance their lifestyle and creating our globe in structure of one big family in its true sense.We also export other products of any category, it will be a pleasure if you contact us!",
+    second_p : "Thank you for visiting and connecting with us, we assure you for the best quality of food products and services. You would be glad doing business with us. We supply agriculture food products which are produced in India. We are grateful for connecting communities🫂 by exporting commodities🌱 and building relations with other nations 🚢 Come lets shake hands and make this world a global village!"
+  }
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Info image='Who we are.jpg' title='WHO WE ARE' des={obj.first_p}/>
+      <Info image='message.jpg' title='MESSAGE FOR YOU' des={obj.second_p}/>
+      <Footer/>
+    </>
   )
 }
 

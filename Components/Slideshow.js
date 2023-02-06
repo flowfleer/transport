4fdec83d21c740736e829b940d93af2820b02fd7
Rupen Bhimani <rupenbhimani109@gmail.com>
2023-02-06 21:29:50 +0530

    fix bugs, add multi content in slider

--- Components/Slideshow.js
+++ Components/Slideshow.js
@@ -2,22 +2,42 @@ import React from "react";
 //These are Third party packages for smooth slideshow
 import { Zoom } from "react-slideshow-image";
 import "react-slideshow-image/dist/styles.css";
-import Image from 'next/image'
+import Image from 'next/image';
+import truck1 from '../public/pic/WhatsApp Image truck1.jpeg'
+import truck2 from '../public/pic/WhatsApp Image truck2.jpeg'
+import truck3 from '../public/pic/WhatsApp Image truck3.jpeg'
 
 const Slideshow = () => {
   //Array of Images
-  const images = [
+  // const images = [
 
-    "/robson-hatsukami-morgan-NKr0qBAkU4s-unsplash.jpg",
-    "/william-william-NndKt2kF1L4-unsplash.jpg",
-    // "/aron-yigin-sNY6B9NsPP8-unsplash.jpg",
-    "/christopher-paul-high-O8dcG8oniJU-unsplash.jpg",
+  //   "/pic/WhatsApp Image truck1.jpeg",
+  //   "/pic/WhatsApp Image truck2.jpeg",
+  //   "/pic/WhatsApp Image truck3.jpeg",
+
+  // ];
+  const content = [
+    {
+      title: "AKSHAR INTERNATIONAL",
+      description: "CNC Machine Conveyor, Conveyor Chain",
+      image: truck1
+    },
+    {
+      title: "AKSHAR INTERNATIONAL",
+      description: "CNC/VMC Machine Conveyor",
+      image: truck2
+    },
+    {
+      title: "AKSHAR INTERNATIONAL",
+      description: "•Chip Conveyor •Slat Conveyour ",
+      image: truck3
+    },
   ];
 
   //These are custom properties for zoom effect while slide-show
   const zoomInProperties = {
     indicators: true,
-    // scale: 1.2,
+    scale: 1.2,
     duration: 2000,
     transitionDuration: 500,
     infinite: true,
@@ -48,10 +68,12 @@ const Slideshow = () => {
   return (
     <div className="m-6">
       <Zoom {...zoomInProperties}>
-        {images.map((each, index) => (
-          <div key={index} className="flex items-center justify-center lg:mt-14">
-            <Image width={1400} height={1400} className="lg:w-10/12 lg:h-[500px] w-full h-screen object-cover bg-top rounded-lg shadow-xl" src={each}/>
-          <p className="fixed md:text-7xl text-4xl font-extrabold ml-4">AKSHAR INTERNATIONAL</p>
+        {content.map((each, index) => (
+          <div key={index} className=" flex items-center justify-center lg:mt-14">
+            <Image width={1400} height={1400} className="lg:w-10/12 lg:h-[500px] w-full h-screen object-cover bg-top rounded-lg shadow-xl" src={each.image}/>
+            <p className="absolute md:text-7xl text-4xl font-extrabold ml-4 text-white">{each.title}</p>
+            <p className="absolute mt-32 md:text-3xl text-4xl font-extrabold ml-4 text-white">{each.description}</p>
+
           </div>
         ))}
       </Zoom>

04e1ae91a4084c94cbfccc8cebd8c28b876364b5
Raj <raj.ghodasara04@gmail.com>
2023-02-23 18:53:31 +0530

    logo added

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Image from 'next/image';
-import logo from '../public/logo.png';
+import logo from '../public/logo.jpg';
 
 
 export default function NavBar() {

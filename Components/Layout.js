c9a89c44a571c44145336730d6046c3f2fd825aa
Rupen Bhimani <rupenbhimani109@gmail.com>
2023-03-12 14:10:36 +0530

    copyright and favicon

--- Components/Layout.js
+++ Components/Layout.js
@@ -45,7 +45,14 @@ const Layout = () => {
             <Container/>
             <Choose/>
             <Information />
-            <Footer/>
+                <Footer />
+                <div class="bg-neutral-200 p-6 text-center dark:bg-neutral-700">
+                    <span>© 2023 Copyright : </span>
+                    <a
+                        class="font-semibold text-neutral-800 dark:text-neutral-400"
+                    >Akshar International</a
+                    >
+                </div>
         </div>
         </>
     );

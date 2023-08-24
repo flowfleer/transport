9ab178948945584b9e7ccbed1ac33a08a92a4606
Raj <raj.ghodasara04@gmail.com>
2023-08-24 18:57:59 +0530

    container background image fix

--- Components/TestBox.js
+++ Components/TestBox.js
@@ -3,6 +3,10 @@ import Image from 'next/image';
 import Popup from 'reactjs-popup'; 
 
 const TestBox = ({ title, desc, img, readMore }) => {
+    // const idGen = () =>{
+    //     let id=-1;
+    //     return id++;
+    // }
     return (
         <>
             <section className="text-gray-600 body-font">
@@ -15,7 +19,7 @@ const TestBox = ({ title, desc, img, readMore }) => {
                                     <h1 className="title-font md:text-2xl text-xl font-medium text-white mb-1">{title}</h1>
                                     <div className="w-20 h-1 bg-cyan-400 rounded mt-2"></div>
                                     <p className="leading-relaxed md:text-lg lg:mt-4 mb-3 text-white">{desc}</p>
-                                    <Popup trigger={<button className='text-primary'> Read More </button>} modal nested>{
+                                    <Popup trigger={<button className='text-primary' /*aria-describedby={`popup-${idGen}`}*/> Read More </button>} modal nested>{
                                         close => (
                                                 <div className= "fixed top-0 left-0 w-full h-screen bg-gray-900 opacity-90">
                                                     <div className='text-white relative w-[50%] h-fit mx-auto top-[10%] bg-gray-900 text-sm md:text-base xl:text-lg'>
